c94fc0b61b9ed1c20803daf1cde6280a9757d373
Frederik Petersen <fred.p@live.dk>
2022-09-09 14:00:53 +0200

    Resolutions test

--- Assignment01.Tests/RegExprTests.cs
+++ Assignment01.Tests/RegExprTests.cs
@@ -10,4 +10,13 @@ public class RegExprTests
 
         result.Should().BeEquivalentTo(new List<string>(){"Op", "til", "flere", "ord", "Et", "par","ord","her","også"});
     }
+
+    [Fact]
+    public void Resolutions_when_given_lines_of_resolutions_returns_tuples_of_dimensions(){
+        var resolutions = new List<string>(){"1920x1080", "1024x768, 800x600", "1280x960"};
+
+        var result = RegExpr.Resolutions(resolutions);
+
+        result.Should().BeEquivalentTo(new List<(int,int)>(){(1920, 1080),(1024,768),(800,600),(1280,960)});
+    }
 }
