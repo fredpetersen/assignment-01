34e64bb8e87a863ca91319f3614e60c4707f4dae
Frederik Petersen <fred.p@live.dk>
2022-09-09 13:51:02 +0200

    Splitlines test

--- Assignment01.Tests/RegExprTests.cs
+++ Assignment01.Tests/RegExprTests.cs
@@ -2,4 +2,12 @@ namespace Assignment01.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void SplitLines_when_given_lines_returns_words(){
+        var lines = new List<string>{"Op til flere ord", "Et par ord her også"};
+
+        var result = RegExpr.SplitLine(lines);
+
+        result.Should().BeEquivalentTo(new List<string>(){"Op", "til", "flere", "ord", "Et", "par","ord","her","også"});
+    }
 }
