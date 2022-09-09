c207bf687c1a332afb7061eeb8794ad3158a4ccf
Adrian Kari <adrka@itu.dk>
2022-09-09 14:19:05 +0200

    Implemented SplitLine & Resolutions

--- Assignment01/RegExpr.cs
+++ Assignment01/RegExpr.cs
@@ -2,9 +2,27 @@ namespace Assignment01;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+        foreach(var line in lines){
+            foreach(var word in line.Split(" "))
+            {
+                yield return word;
+            }
+        }
+    }
 
     public static IEnumerable<(int width, int height)> Resolutions(IEnumerable<string> resolutions) => throw new NotImplementedException();
+    {
+
+        var pattern = @"(?<width>\d+)x(?<height>\d+)";
+
+
+        foreach(var resolution in resolutions.Split(", "))
+        {
+            yield return resolution
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
